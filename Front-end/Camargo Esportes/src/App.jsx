1ad69cf7eb59b958430e383e7007be4660273db9
Lucas-SousaS <lucassousa4019@gmail.com>
2024-11-13 10:26:21 -0300

    feat: create api endpoint

--- Front-end/Camargo Esportes/src/App.jsx
+++ Front-end/Camargo Esportes/src/App.jsx
@@ -1,34 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [data, setData] = useState([])
+  const url = "http://localhost/CE-Camargo-Esportes-/Back-end/get_tasks.php"
+  useEffect(() => {
+    fetch(url).then(res => res.json()).then(data => setData(data))
+  }, [])
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      {data.map((item, index) => (
+        <div key={index}>
+          <h1 className='font-semibold text-lg' >{item.titulo}</h1>
+          <p className='font-normal text-sm'>{item.conteudo}</p>
+        </div>
+      ))}
+    </div>
   )
 }
 
