bffb4f9d5b615921e1e9d45c6141e63db5eed851
Lucas-SousaS <lucassousa4019@gmail.com>
2024-11-15 20:08:07 -0300

    feat: adding validations

--- Back-end/post_data.php
+++ Back-end/post_data.php
@@ -3,9 +3,14 @@ include('database.php');
 
 $dados = json_decode(file_get_contents("php://input"), true);
 
-$nome = $dados['nome'] ?? null;
+if (empty($dados['nome']) || !is_string($dados['nome']) || strlen($dados['nome']) > 100) {
+    $nome = null;
+} else {
+    $nome = htmlspecialchars(trim($dados['nome']));
+}
 $email = $dados['email'] ?? null;
 $senha = $dados['senha'] ?? null;
+
 $data_nascimento = $dados['data_nascimento'] ?? null;
 $genero = $dados['genero'] ?? null;
 $cidade = $dados['cidade'] ?? null;
@@ -29,10 +34,12 @@ try {
     if ($stmt->execute()) {
         header('Content-Type: application/json');
         echo json_encode(["status" => "success", "message" => "Usuário cadastrado com sucesso"]);
+        echo $data_nascimento;
     } else {
         echo json_encode(["status" => "error", "message" => "Erro ao cadastrar usuário"]);
     }
 } catch (PDOException $e) {
     echo json_encode(['error' => $e->getMessage()]);
 }
+
 ?>
