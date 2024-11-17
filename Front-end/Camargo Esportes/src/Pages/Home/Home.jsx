b95a826594894731b148c4ac41e2be8384902cbd
Lucas-SousaS <lucassousa4019@gmail.com>
2024-11-16 23:54:36 -0300

    feat: create news page

--- Front-end/Camargo Esportes/src/Pages/Home/Home.jsx
+++ Front-end/Camargo Esportes/src/Pages/Home/Home.jsx
@@ -6,33 +6,54 @@ import Footer from "../../components/Footer/Footer";
 function Home() {
   const [data, setData] = useState([]);
   const url = "http://localhost/CE-Camargo-Esportes/Back-end/get_tasks.php";
+
   useEffect(() => {
     fetch(url)
       .then((res) => res.json())
-      .then((data) => setData(data));
+      .then((data) => {
+        const formattedData = data.map(item => {
+          if (typeof item.imagens === 'string') {
+            item.imagens = JSON.parse(item.imagens);
+          }
+          return item;
+        });
+        setData(formattedData);
+      });
   }, []);
 
- 
   return (
     <>
-    
       <Header />
 
-      <div className="flex flex-col w-full left-10 items-center mt-10 mb-10">
+      <div className="flex flex-col w-full left-10 items-center mt-10 mb-10 min-h-screen">
         <div className="flex flex-col w-1/2 left-10 items-start ">
           {data.map((item, index) => (
-            <Link to={`/news/${item.id}`} className="flex items-center gap-6 border-b-2 p-8" key={index}>
-              <img className="w-[400px] h-60 object-cover" src={item.imagem_url} alt="Luan Melancia" />
-              <div className="flex flex-col gap-2">
-                <h1 className="font-bold text-2xl text-[#06aa48]" >{item.titulo}</h1>
-                <p className="font-normal text-md ">{item.conteudo}</p>
-                <span className="font normal text-sm">{item.data_publicacao} | {item.categoria}</span>
+            <Link
+              to={`/news/${item.id}`}
+              className="w-[100%] flex items-center justify-between gap-6 border-b-2 py-8"
+              key={index}
+            >
+              <div className="">
+                {/* Aqui você acessa a imagem principal do objeto imagens */}
+                <img
+                  className="w-[300px] object-cover rounded-sm"
+                  src={item.imagens.imagem_principal}
+                  alt={item.titulo} // Use o título como alt para acessibilidade
+                />
+              </div>
+
+              <div className="w-1/2 flex flex-col gap-2">
+                <h1 className="font-bold text-2xl text-[#06aa48]">
+                  {item.titulo}
+                </h1>
+                <p className="font-normal text-md truncate">{item.conteudo}</p>
+                <span className="font normal text-sm">
+                  {item.data_publicacao} | {item.categoria}
+                </span>
               </div>
             </Link>
           ))}
         </div>
-
-        
       </div>
 
       <Footer />
--- Front-end/Camargo Esportes/src/Pages/News/News.jsx
+++ Front-end/Camargo Esportes/src/Pages/News/News.jsx
@@ -8,6 +8,8 @@ function News() {
   const [noticia, setNoticia] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [images, setImages] = useState([]);
+  const [materiaF, setMateriaF] = useState([]);
 
   useEffect(() => {
     const url = `http://localhost/CE-Camargo-Esportes/Back-end/get_news.php?id=${id}`;
@@ -23,6 +25,13 @@ function News() {
         if (data.error) {
           setError(data.error);
         } else {
+          const imagesNew = JSON.parse(data.imagens);
+          setImages(imagesNew);
+
+          const string = data.materia_completa;
+          const paragraphs = string.split(/(?<=\w[.!?])\s+/);
+          setMateriaF(paragraphs);
+
           setNoticia(data);
         }
       })
@@ -48,16 +57,44 @@ function News() {
       <div className="min-h-screen w-screen flex items-start justify-center py-20">
         {noticia ? (
           <div className="flex flex-col w-1/2">
-            <div className="flex flex-col gap-4 border-b-2 pb-3 border-[#5555552d]">
-              <h1 className="font-bold text-3xl">{noticia.titulo}</h1>
+            <div className="flex flex-col gap-4 border-b-2 pb-3 border-[#5555552d] mb-14">
+              <h1 className="font-bold text-5xl">{noticia.titulo}</h1>
               <p className="font-normal text-[#555555]">{noticia.conteudo}</p>
-              <p className="font-bold text-[#555555]">Por {noticia.autor} <span>-</span> <span className="font-normal text-xs text-[#555555]">{noticia.data_publicacao}</span></p>
+              <p className="font-bold text-[#555555]">
+                Por {noticia.autor} <span>-</span>{" "}
+                <span className="font-normal text-xs text-[#555555]">
+                  {noticia.data_publicacao}
+                </span>
+              </p>
+            </div>
+            <div className="flex items-center justify-center w-[100%]">
+              <img src={`${images.imagem_principal}`} alt="vila belmiro" />
             </div>
 
-            <img src={noticia.imagem_url} />
+            <div className="flex flex-col gap-2 mt-5">
+              {materiaF.map((item, index) => (
+                <div key={index} className="flex flex-col gap-5">
+                  <h1 className="font-normal text-xl text-[#333333]">{item}</h1>
+                  <div className="flex w-[100%] items-center justify-center">
+                    {index == 3 && (
+                      <img
+                        className="max-h-[600px] max-w-[430px]"
+                        src={`${images.imagem_adicional_1}`}
+                        alt={`Imagem ${index + 1}`}
+                      />
+                    )}
+                  </div>
+                </div>
+              ))}
+            </div>
           </div>
         ) : (
-          <p>Notícia não encontrada.</p>
+          <div className="min-h-screen w-screen flex items-start justify-center py-20">
+            <div className="flex flex-col items-center justify-center">
+              <h1>Opps...</h1>
+              <p>página não encontrada.</p>
+            </div>
+          </div>
         )}
       </div>
       <Footer />
